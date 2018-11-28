63698d39f828ad00ea5e5ac370e8d13ab21e1c78
Manu <emanuelnazare@yahoo.ro>
2018-11-28 10:34:43 +0200

    Updates

--- precache-manifest.9d8c1d0c140891682edc268f085ad3ac.js
+++ precache-manifest.9d8c1d0c140891682edc268f085ad3ac.js
@@ -32,12 +32,12 @@ self.__precacheManifest = [
     "url": "/black-dashboard-react/static/media/emilyz.9fcf69e5.jpg"
   },
   {
-    "revision": "f0b489a5dbbff08833d21024f9fcbd4e",
-    "url": "/black-dashboard-react/static/media/nucleo.f0b489a5.woff"
-  },
-  {
     "revision": "5987dd12fea78ce5f97ae601b08ec03c",
     "url": "/black-dashboard-react/static/media/nucleo.5987dd12.woff2"
+  },
+  {
+    "revision": "f0b489a5dbbff08833d21024f9fcbd4e",
+    "url": "/black-dashboard-react/static/media/nucleo.f0b489a5.woff"
   },
   {
     "revision": "03ef1918e505c3e3471f9369ef7a638f",
