898db10118ec6620bb55cb1a1d9efc75369c3c5d
Manu <emanuelnazare@yahoo.ro>
2018-11-28 11:42:20 +0200

    - Small live changes

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { createBrowserHistory } from "history";
+import { createHashHistory } from "history";
 import { Router, Route, Switch, Redirect } from "react-router-dom";
 
 import AdminLayout from "layouts/Admin/Admin.jsx";
@@ -13,7 +13,7 @@ import "assets/scss/black-dashboard-react.scss";
 import "assets/demo/demo.css";
 import "assets/css/nucleo-icons.css";
 
-const hist = createBrowserHistory();
+const hist = createHashHistory();
 
 ReactDOM.render(
   <Router history={hist}>
