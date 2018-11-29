64181f4dac2a70e21bb8385a63146b422ca3ea6f
Manu <emanuelnazare@yahoo.ro>
2018-11-29 12:04:15 +0200

    Updates

--- precache-manifest.bec0b1542dee36e465aaf43fdca27d11.js
+++ precache-manifest.bec0b1542dee36e465aaf43fdca27d11.js
@@ -8,8 +8,8 @@ self.__precacheManifest = [
     "url": "/black-dashboard-react/static/css/main.717aa147.chunk.css"
   },
   {
-    "revision": "b17a118e13e53558658b681a0ebdad82",
-    "url": "/black-dashboard-react/static/media/nucleo.b17a118e.ttf"
+    "revision": "03ef1918e505c3e3471f9369ef7a638f",
+    "url": "/black-dashboard-react/static/media/nucleo.03ef1918.eot"
   },
   {
     "revision": "f1913036c31297287dd1",
@@ -32,23 +32,23 @@ self.__precacheManifest = [
     "url": "/black-dashboard-react/static/media/emilyz.9fcf69e5.jpg"
   },
   {
-    "revision": "5987dd12fea78ce5f97ae601b08ec03c",
-    "url": "/black-dashboard-react/static/media/nucleo.5987dd12.woff2"
-  },
-  {
     "revision": "f0b489a5dbbff08833d21024f9fcbd4e",
     "url": "/black-dashboard-react/static/media/nucleo.f0b489a5.woff"
   },
   {
-    "revision": "03ef1918e505c3e3471f9369ef7a638f",
-    "url": "/black-dashboard-react/static/media/nucleo.03ef1918.eot"
+    "revision": "5987dd12fea78ce5f97ae601b08ec03c",
+    "url": "/black-dashboard-react/static/media/nucleo.5987dd12.woff2"
+  },
+  {
+    "revision": "b17a118e13e53558658b681a0ebdad82",
+    "url": "/black-dashboard-react/static/media/nucleo.b17a118e.ttf"
   },
   {
     "revision": "f1913036c31297287dd1",
     "url": "/black-dashboard-react/static/css/1.60530448.chunk.css"
   },
   {
-    "revision": "164ff026bbe911d822655bafcff82295",
+    "revision": "97d5df46a6ba7ce3fb8e8c1e3fd10eaf",
     "url": "/black-dashboard-react/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/black-dashboard-react/precache-manifest.fe14bac5bf289a779c676538f56f3b9e.js"
+  "/black-dashboard-react/precache-manifest.bec0b1542dee36e465aaf43fdca27d11.js"
 );
 
 workbox.clientsClaim();
