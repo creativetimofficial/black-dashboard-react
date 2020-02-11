66fae6af0fff45836a9e00e32c76d72c3cc04491
EINazare <emanuelioannazare@gmail.com>
2020-02-11 15:54:44 +0200

    Updates

---
+++ precache-manifest.3ded57197233c6877045fac397c39e6e.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = [
+  {
+    "revision": "313b57a2c6bc4d9d7459",
+    "url": "/black-dashboard-react/static/css/main.aa14add7.chunk.css"
+  },
+  {
+    "revision": "313b57a2c6bc4d9d7459",
+    "url": "/black-dashboard-react/static/js/main.313b57a2.chunk.js"
+  },
+  {
+    "revision": "6ccc6352f87cb5e72fef",
+    "url": "/black-dashboard-react/static/css/1.20f62aae.chunk.css"
+  },
+  {
+    "revision": "6ccc6352f87cb5e72fef",
+    "url": "/black-dashboard-react/static/js/1.6ccc6352.chunk.js"
+  },
+  {
+    "revision": "b46a1e60e5b4cb2d0205",
+    "url": "/black-dashboard-react/static/js/runtime~main.b46a1e60.js"
+  },
+  {
+    "revision": "eb6be414b75c412c13eeee2d2e8390cb",
+    "url": "/black-dashboard-react/static/media/react-logo.eb6be414.png"
+  },
+  {
+    "revision": "bd6820f1a4015e9645e470ea8e7b1501",
+    "url": "/black-dashboard-react/static/media/anime3.bd6820f1.png"
+  },
+  {
+    "revision": "9fcf69e5359987c9b1545aae1c2c5faa",
+    "url": "/black-dashboard-react/static/media/emilyz.9fcf69e5.jpg"
+  },
+  {
+    "revision": "f0b489a5dbbff08833d21024f9fcbd4e",
+    "url": "/black-dashboard-react/static/media/nucleo.f0b489a5.woff"
+  },
+  {
+    "revision": "03ef1918e505c3e3471f9369ef7a638f",
+    "url": "/black-dashboard-react/static/media/nucleo.03ef1918.eot"
+  },
+  {
+    "revision": "5987dd12fea78ce5f97ae601b08ec03c",
+    "url": "/black-dashboard-react/static/media/nucleo.5987dd12.woff2"
+  },
+  {
+    "revision": "b17a118e13e53558658b681a0ebdad82",
+    "url": "/black-dashboard-react/static/media/nucleo.b17a118e.ttf"
+  },
+  {
+    "revision": "7159a4d36f44b6524862269385c0a006",
+    "url": "/black-dashboard-react/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/black-dashboard-react/precache-manifest.4b523531c04eadc1e2138e988a211071.js"
+  "/black-dashboard-react/precache-manifest.3ded57197233c6877045fac397c39e6e.js"
 );
 
 workbox.clientsClaim();
@@ -30,5 +30,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute("/black-dashboard-react/index.html", {
   
-  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
