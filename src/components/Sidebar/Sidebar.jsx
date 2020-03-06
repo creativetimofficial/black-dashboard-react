be226d89ccbc6e4cc7b130252997b85616f353e2
EINazare <emanuelioannazare@gmail.com>
2020-03-06 16:38:36 +0200

    feature: add upgrade to pro button

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -25,7 +25,7 @@ import { PropTypes } from "prop-types";
 import PerfectScrollbar from "perfect-scrollbar";
 
 // reactstrap components
-import { Nav } from "reactstrap";
+import { Nav, NavLink as ReactstrapNavLink } from "reactstrap";
 
 var ps;
 
@@ -137,6 +137,16 @@ class Sidebar extends React.Component {
                 </li>
               );
             })}
+            <li
+              className="active-pro"
+            >
+              <ReactstrapNavLink
+                href="https://www.creative-tim.com/product/black-dashboard-pro-react?ref=bdr-user-archive-sidebar-upgrade-pro"
+              >
+                <i className="tim-icons icon-spaceship" />
+                <p>Upgrade to PRO</p>
+              </ReactstrapNavLink>
+            </li>
           </Nav>
         </div>
       </div>
