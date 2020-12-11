a00c3e8875a50c8d4362f9ad965c0df647f0a067
EINazare <emanuelioannazare@gmail.com>
2020-12-11 14:58:23 +0200

    refactor: add hooks support

--- src/layouts/RTL/RTL.js
+++ src/layouts/RTL/RTL.js
@@ -16,7 +16,7 @@
 
 */
 import React from "react";
-import { Route, Switch } from "react-router-dom";
+import { Route, Switch, useLocation } from "react-router-dom";
 // javascript plugin used to create scrollbars on windows
 import PerfectScrollbar from "perfect-scrollbar";
 
@@ -33,21 +33,17 @@ import { BackgroundColorContext } from "contexts/BackgroundColorContext";
 
 var ps;
 
-class Admin extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      backgroundColor: "blue",
-      sidebarOpened:
-        document.documentElement.className.indexOf("nav-open") !== -1,
-    };
-    this.mainPanel = React.createRef();
-  }
-  componentDidMount() {
+function RTL(props) {
+  const location = useLocation();
+  const mainPanelRef = React.useRef(null);
+  const [sidebarOpened, setsidebarOpened] = React.useEffect(
+    document.documentElement.className.indexOf("nav-open") !== -1
+  );
+  React.useEffect(() => {
     if (navigator.platform.indexOf("Win") > -1) {
       document.documentElement.className += " perfect-scrollbar-on";
       document.documentElement.classList.remove("perfect-scrollbar-off");
-      ps = new PerfectScrollbar(this.mainPanel.current, {
+      ps = new PerfectScrollbar(mainPanelRef.current, {
         suppressScrollX: true,
       });
       let tables = document.querySelectorAll(".table-responsive");
@@ -67,39 +63,40 @@ class Admin extends React.Component {
     link.href =
       "https://cdnjs.cloudflare.com/ajax/libs/bootstrap-rtl/3.4.0/css/bootstrap-rtl.css";
     head.appendChild(link);
-  }
-  componentWillUnmount() {
-    if (navigator.platform.indexOf("Win") > -1) {
-      ps.destroy();
-      document.documentElement.className += " perfect-scrollbar-off";
-      document.documentElement.classList.remove("perfect-scrollbar-on");
-    }
-    // when we exit this page, we need to delete the classes .rtl and .menu-on-right
-    // from the body tag
-    document.body.classList.remove("rtl", "menu-on-right");
-    // we also need to delete the rtl bootstrap, so it does not break the other pages
-    // that do not make use of rtl
-    document.getElementById("rtl-id").remove();
-  }
-  componentDidUpdate(e) {
-    if (e.history.action === "PUSH") {
+    // Specify how to clean up after this effect:
+    return function cleanup() {
       if (navigator.platform.indexOf("Win") > -1) {
-        let tables = document.querySelectorAll(".table-responsive");
-        for (let i = 0; i < tables.length; i++) {
-          ps = new PerfectScrollbar(tables[i]);
-        }
+        ps.destroy();
+        document.documentElement.classList.add("perfect-scrollbar-off");
+        document.documentElement.classList.remove("perfect-scrollbar-on");
       }
-      document.documentElement.scrollTop = 0;
-      document.scrollingElement.scrollTop = 0;
-      this.mainPanel.current.scrollTop = 0;
+      // when we exit this page, we need to delete the classes .rtl and .menu-on-right
+      // from the body tag
+      document.body.classList.remove("rtl", "menu-on-right");
+      // we also need to delete the rtl bootstrap, so it does not break the other pages
+      // that do not make use of rtl
+      document.getElementById("rtl-id").remove();
+    };
+  });
+  React.useEffect(() => {
+    if (navigator.platform.indexOf("Win") > -1) {
+      let tables = document.querySelectorAll(".table-responsive");
+      for (let i = 0; i < tables.length; i++) {
+        ps = new PerfectScrollbar(tables[i]);
+      }
     }
-  }
+    document.documentElement.scrollTop = 0;
+    document.scrollingElement.scrollTop = 0;
+    if (mainPanelRef.current) {
+      mainPanelRef.current.scrollTop = 0;
+    }
+  }, [location]);
   // this function opens and closes the sidebar on small devices
-  toggleSidebar = () => {
+  const toggleSidebar = () => {
     document.documentElement.classList.toggle("nav-open");
-    this.setState({ sidebarOpened: !this.state.sidebarOpened });
+    setsidebarOpened(!sidebarOpened);
   };
-  getRoutes = (routes) => {
+  const getRoutes = (routes) => {
     return routes.map((prop, key) => {
       if (prop.layout === "/rtl") {
         return (
@@ -114,62 +111,50 @@ class Admin extends React.Component {
       }
     });
   };
-  getBrandText = (path) => {
+  const getBrandText = (path) => {
     for (let i = 0; i < routes.length; i++) {
-      if (
-        this.props.location.pathname.indexOf(
-          routes[i].layout + routes[i].path
-        ) !== -1
-      ) {
-        return routes[i].rtlName || routes[i].name;
+      if (location.pathname.indexOf(routes[i].layout + routes[i].path) !== -1) {
+        return routes[i].name;
       }
     }
     return "Brand";
   };
-  render() {
-    return (
-      <>
-        <div className="wrapper">
-          <Sidebar
-            {...this.props}
-            routes={routes}
-            bgColor={this.state.backgroundColor}
-            rtlActive
-            logo={{
-              outterLink: "https://www.creative-tim.com/",
-              text: "الإبداعية تيم",
-              imgSrc: logo,
-            }}
-            toggleSidebar={this.toggleSidebar}
-          />
-          <div
-            className="main-panel"
-            ref={this.mainPanel}
-            data={this.state.backgroundColor}
-          >
-            <RTLNavbar
-              {...this.props}
-              brandText={this.getBrandText(this.props.location.pathname)}
-              toggleSidebar={this.toggleSidebar}
-              sidebarOpened={this.state.sidebarOpened}
-            />
-            <Switch>{this.getRoutes(routes)}</Switch>
-            {
-              // we don't want the Footer to be rendered on map page
-              this.props.location.pathname.indexOf("maps") !== -1 ? null : (
-                <Footer fluid />
-              )
-            }
-          </div>
-        </div>
-        <BackgroundColorContext.Consumer>
-          {({ color, changeColor }) => (
-            <FixedPlugin bgColor={color} handleBgClick={changeColor} />
-          )}
-        </BackgroundColorContext.Consumer>
-      </>
-    );
-  }
+  return (
+    <>
+      <BackgroundColorContext.Consumer>
+        {({ color, changeColor }) => (
+          <React.Fragment>
+            <div className="wrapper">
+              <Sidebar
+                {...props}
+                routes={routes}
+                rtlActive
+                logo={{
+                  outterLink: "https://www.creative-tim.com/",
+                  text: "الإبداعية تيم",
+                  imgSrc: logo,
+                }}
+                toggleSidebar={toggleSidebar}
+              />
+              <div className="main-panel" ref={mainPanelRef} data={color}>
+                <RTLNavbar
+                  {...props}
+                  brandText={getBrandText(location.pathname)}
+                  toggleSidebar={toggleSidebar}
+                  sidebarOpened={sidebarOpened}
+                />
+                <Switch>{getRoutes(routes)}</Switch>
+                {
+                  // we don't want the Footer to be rendered on map page
+                  location.pathname == "/admin/maps" ? null : <Footer fluid />
+                }
+              </div>
+            </div>
+          </React.Fragment>
+        )}
+      </BackgroundColorContext.Consumer>
+    </>
+  );
 }
 
-export default Admin;
+export default RTL;
