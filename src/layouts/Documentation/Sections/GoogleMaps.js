d0452d063349b2262db310d13428302042d46738
EINazare <emanuelioannazare@gmail.com>
2020-12-11 15:50:28 +0200

    feature: add google maps and deprecate react google maps

--- src/layouts/Documentation/Sections/GoogleMaps.js
+++ src/layouts/Documentation/Sections/GoogleMaps.js
@@ -17,234 +17,52 @@
 */
 import React from "react";
 import { Prism as SyntaxHighlighter } from "react-syntax-highlighter";
-import { prism } from "react-syntax-highlighter/styles/prism";
-import {
-  withScriptjs,
-  withGoogleMap,
-  GoogleMap,
-  Marker
-} from "react-google-maps";
-
-const codeImport = `import {
-  withScriptjs,
-  withGoogleMap,
-  GoogleMap,
-  Marker,
-} from "react-google-maps"`;
-
-const codeExample = `const MapWrapper = withScriptjs(withGoogleMap(props =>
-    <GoogleMap
-        defaultZoom={13}
-        defaultCenter={{ lat: 40.748817, lng: -73.985428 }}
-        defaultOptions={{
-            scrollwheel: false,
-            styles: [{"featureType":"water","elementType":"geometry","stylers":[{"color":"#e9e9e9"},{"lightness":17}]},{"featureType":"landscape","elementType":"geometry","stylers":[{"color":"#f5f5f5"},{"lightness":20}]},{"featureType":"road.highway","elementType":"geometry.fill","stylers":[{"color":"#ffffff"},{"lightness":17}]},{"featureType":"road.highway","elementType":"geometry.stroke","stylers":[{"color":"#ffffff"},{"lightness":29},{"weight":0.2}]},{"featureType":"road.arterial","elementType":"geometry","stylers":[{"color":"#ffffff"},{"lightness":18}]},{"featureType":"road.local","elementType":"geometry","stylers":[{"color":"#ffffff"},{"lightness":16}]},{"featureType":"poi","elementType":"geometry","stylers":[{"color":"#f5f5f5"},{"lightness":21}]},{"featureType":"poi.park","elementType":"geometry","stylers":[{"color":"#dedede"},{"lightness":21}]},{"elementType":"labels.text.stroke","stylers":[{"visibility":"on"},{"color":"#ffffff"},{"lightness":16}]},{"elementType":"labels.text.fill","stylers":[{"saturation":36},{"color":"#333333"},{"lightness":40}]},{"elementType":"labels.icon","stylers":[{"visibility":"off"}]},{"featureType":"transit","elementType":"geometry","stylers":[{"color":"#f2f2f2"},{"lightness":19}]},{"featureType":"administrative","elementType":"geometry.fill","stylers":[{"color":"#fefefe"},{"lightness":20}]},{"featureType":"administrative","elementType":"geometry.stroke","stylers":[{"color":"#fefefe"},{"lightness":17},{"weight":1.2}]}] }}
-    >
-        <Marker
-            position={{ lat: 40.748817, lng: -73.985428 }}
-        />
-    </GoogleMap>
-));`;
-
-const codeExampleRender = `<div id="map" style={{position: "relative", overflow: "hidden"}}>
-    <MapWrapper
-        googleMapURL="https://maps.googleapis.com/maps/api/js?key=YOUR_KEY_HERE"
-        loadingElement={<div style={{ height: \`100%\` }} />}
-        containerElement={<div style={{ height: \`100%\` }} />}
-        mapElement={<div style={{ height: \`100%\` }} />}
-    />
-</div>`;
-
-const MapWrapper = withScriptjs(
-  withGoogleMap(props => (
-    <GoogleMap
-      defaultZoom={13}
-      defaultCenter={{ lat: 40.748817, lng: -73.985428 }}
-      defaultOptions={{
-        scrollwheel: false,
-        styles: [
-          {
-            featureType: "water",
-            elementType: "geometry",
-            stylers: [{ color: "#e9e9e9" }, { lightness: 17 }]
-          },
-          {
-            featureType: "landscape",
-            elementType: "geometry",
-            stylers: [{ color: "#f5f5f5" }, { lightness: 20 }]
-          },
-          {
-            featureType: "road.highway",
-            elementType: "geometry.fill",
-            stylers: [{ color: "#ffffff" }, { lightness: 17 }]
-          },
-          {
-            featureType: "road.highway",
-            elementType: "geometry.stroke",
-            stylers: [{ color: "#ffffff" }, { lightness: 29 }, { weight: 0.2 }]
-          },
-          {
-            featureType: "road.arterial",
-            elementType: "geometry",
-            stylers: [{ color: "#ffffff" }, { lightness: 18 }]
-          },
-          {
-            featureType: "road.local",
-            elementType: "geometry",
-            stylers: [{ color: "#ffffff" }, { lightness: 16 }]
-          },
-          {
-            featureType: "poi",
-            elementType: "geometry",
-            stylers: [{ color: "#f5f5f5" }, { lightness: 21 }]
-          },
-          {
-            featureType: "poi.park",
-            elementType: "geometry",
-            stylers: [{ color: "#dedede" }, { lightness: 21 }]
-          },
-          {
-            elementType: "labels.text.stroke",
-            stylers: [
-              { visibility: "on" },
-              { color: "#ffffff" },
-              { lightness: 16 }
-            ]
-          },
-          {
-            elementType: "labels.text.fill",
-            stylers: [
-              { saturation: 36 },
-              { color: "#333333" },
-              { lightness: 40 }
-            ]
-          },
-          { elementType: "labels.icon", stylers: [{ visibility: "off" }] },
-          {
-            featureType: "transit",
-            elementType: "geometry",
-            stylers: [{ color: "#f2f2f2" }, { lightness: 19 }]
-          },
-          {
-            featureType: "administrative",
-            elementType: "geometry.fill",
-            stylers: [{ color: "#fefefe" }, { lightness: 20 }]
-          },
-          {
-            featureType: "administrative",
-            elementType: "geometry.stroke",
-            stylers: [{ color: "#fefefe" }, { lightness: 17 }, { weight: 1.2 }]
-          }
-        ]
-      }}
-    >
-      <Marker position={{ lat: 40.748817, lng: -73.985428 }} />
-    </GoogleMap>
-  ))
-);
+import { prism } from "react-syntax-highlighter/dist/styles/prism";
 
 class GoogleMaps extends React.Component {
   render() {
     return (
       <div>
-        <h1 className="bd-title" id="content">
-          React Google Maps v9.4.5
-        </h1>
-        <p className="bd-lead">
-          For maps we've used some components from a react library{" "}
-          <a
-            href="https://github.com/tomchentw/react-google-maps"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            react-google-maps
-          </a>.
+        <h1>Google Maps</h1>
+        <p className="lead">
+          Our Map components uses the Google Maps API and it comes with a custom
+          interface that matches the theme’s colours. Keep reading our Maps
+          examples and learn how to use this plugin.
         </p>
-        <h2>Google API KEY</h2>
-        <p>For this component to work, you will need a google maps API key</p>
-        <p>Follow these steps to get an API key:</p>
-        <ol>
-          <li>
-            Go to the{" "}
-            <a
-              href="https://console.developers.google.com/flows/enableapi?apiid=maps_backend,geocoding_backend,directions_backend,distance_matrix_backend,elevation_backend,places_backend&reusekey=true"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Google API Console
-            </a>.
-          </li>
-          <li>Create or select a project.</li>
-          <li>Click Continue to enable the API and any related services.</li>
-          <li>On the Credentials page, get an API key.</li>
-          <li>
-            Note: If you have an existing unrestricted API key, or a key with
-            browser restrictions, you may use that key.
-          </li>
-          <li>
-            From the dialog displaying the API key, select Restrict key to set a
-            browser restriction on the API key.
-          </li>
-          <li>
-            In the Key restriction section, select HTTP referrers (web sites),
-            then follow the on-screen instructions to set referrers.
-          </li>
-          <li>
-            (Optional) Enable billing. See{" "}
-            <a
-              href="https://developers.google.com/maps/documentation/javascript/usage"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Usage Limits
-            </a>{" "}
-            for more information.
-          </li>
-        </ol>
+        <h2>Usage</h2>
         <p>
-          After these steps navigate in your project to{" "}
-          <code className="highlighter-rouge">views/Maps/GoogleMaps.js</code>{" "}
-          and{" "}
-          <code className="highlighter-rouge">
-            views/Maps/FullScreenMap.js
-          </code>{" "}
-          and replace the{" "}
-          <code className="highlighter-rouge">YOUR_KEY_HERE</code> with the
-          given API KEY.
+          In order to use this plugin on your page you will need to include the
+          following script inside your <code>public/index.html</code> page,
+          right before the end of the <code>{`</head>`}</code> tag:
         </p>
-        <h2>Example</h2>
-        <p>First of all, you will need the following imports:</p>
         <SyntaxHighlighter language="jsx" style={prism}>
-          {codeImport}
+          {`<script src="https://maps.googleapis.com/maps/api/js?key=YOUR_API_KEY_HERE"></script>`}
         </SyntaxHighlighter>
-        <p>You will have to create a variable for the map:</p>
-        <SyntaxHighlighter language="jsx" style={prism}>
-          {codeExample}
-        </SyntaxHighlighter>
-        <p>After that, in your render method you will have to add:</p>
-        <SyntaxHighlighter language="jsx" style={prism}>
-          {codeExampleRender}
-        </SyntaxHighlighter>
-        <div className="bd-example">
-          <div id="map" style={{ position: "relative", overflow: "hidden" }}>
-            <MapWrapper
-              googleMapURL="https://maps.googleapis.com/maps/api/js?key=AIzaSyBd3PjUqq81lIOfBPYXrQGWwK5T4ystZjA"
-              loadingElement={<div style={{ height: `100%` }} />}
-              containerElement={<div style={{ height: `100%` }} />}
-              mapElement={<div style={{ height: `100%` }} />}
-            />
-          </div>
-        </div>
-        <h2>Props</h2>
+        <h2>Get your API key</h2>
         <p>
-          Please refer to{" "}
+          In order to get your Google Maps API key navigate to the following
+          page:{" "}
           <a
-            href="https://github.com/tomchentw/react-google-maps"
+            className="font-medium text-indigo-600"
+            href="https://developers.google.com/maps/documentation/javascript/get-api-key?ref=creativetim"
+            rel="nofollow"
             target="_blank"
-            rel="noopener noreferrer"
           >
-            react-google-maps documentation
-          </a>.
+            Google Maps
+          </a>
+          . After you’ve got your api key, you will need to replace in the above
+          script, the <code>YOUR_API_KEY_HERE</code> with that key.
+        </p>
+        <p>
+          Read more about Google Maps on their own website:{" "}
+          <a
+            className="font-medium text-indigo-600"
+            href="https://developers.google.com/maps/documentation/javascript/overview?ref=creativetim"
+            rel="nofollow"
+            target="_blank"
+          >
+            here
+          </a>
         </p>
       </div>
     );
