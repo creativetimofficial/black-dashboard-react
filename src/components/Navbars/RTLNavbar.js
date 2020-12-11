187f9ed60551119860a40dceeec97eb97f3d6ed0
EINazare <emanuelioannazare@gmail.com>
2020-12-11 14:20:54 +0200

    fix: solve react-scripts images require

--- src/components/Navbars/RTLNavbar.js
+++ src/components/Navbars/RTLNavbar.js
@@ -171,7 +171,7 @@ class AdminNavbar extends React.Component {
                     onClick={(e) => e.preventDefault()}
                   >
                     <div className="photo">
-                      <img alt="..." src={require("assets/img/anime3.png")} />
+                      <img alt="..." src={require("assets/img/anime3.png").default} />
                     </div>
                     <b className="caret d-none d-lg-block d-xl-block" />
                     <p className="d-lg-none">Log out</p>
