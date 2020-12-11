2baded10116d5f09aea3a41b3b2ec1f1ea5be58d
EINazare <emanuelioannazare@gmail.com>
2020-12-11 16:03:46 +0200

    docs: change date and version

--- src/layouts/Documentation/Header/Header.js
+++ src/layouts/Documentation/Header/Header.js
@@ -1,11 +1,11 @@
 /*!
 
 =========================================================
-* Black Dashboard React v1.1.0
+* Black Dashboard React v1.2.0
 =========================================================
 
 * Product Page: https://www.creative-tim.com/product/black-dashboard-react
-* Copyright 2019 Creative Tim (https://www.creative-tim.com)
+* Copyright 2020 Creative Tim (https://www.creative-tim.com)
 * Licensed under MIT (https://github.com/creativetimofficial/black-dashboard-react/blob/master/LICENSE.md)
 
 * Coded by Creative Tim
@@ -34,7 +34,7 @@ class Header extends React.Component {
         <Nav className="flex-row d-none d-md-flex" navbar>
           <NavItem>
             <NavLink href="#" className="p-2">
-              1.1.0
+              1.2.0
             </NavLink>
           </NavItem>
           <NavItem>
