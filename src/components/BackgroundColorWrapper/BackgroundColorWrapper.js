b184fe219c66bf340f2ff4df9d9576d32ca24fd2
Hadi Alqattan <alqattanhadizaki@gmail.com>
2021-02-15 11:26:25 +0300

    Persist the theme mode and the background color.

--- src/components/BackgroundColorWrapper/BackgroundColorWrapper.js
+++ src/components/BackgroundColorWrapper/BackgroundColorWrapper.js
@@ -1,15 +1,23 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import {
   BackgroundColorContext,
   backgroundColors,
 } from "contexts/BackgroundColorContext";
 
 export default function BackgroundColorWrapper(props) {
-  const [color, setColor] = useState(backgroundColors.blue);
+  const [color, setColor] = useState(
+      () =>
+        JSON.parse(localStorage.getItem('theme-color')) || backgroundColors.blue
+  );
 
   function changeColor(color) {
     setColor(color);
   }
+
+  useEffect(() => {
+      // Persist the theme-color using the browser's localStorage.
+      localStorage.setItem('theme-color', JSON.stringify(color))
+  }, [color])
 
   return (
     <BackgroundColorContext.Provider
--- src/components/ThemeWrapper/ThemeWrapper.js
+++ src/components/ThemeWrapper/ThemeWrapper.js
@@ -2,13 +2,17 @@ import React, { useState, useEffect } from "react";
 import { ThemeContext, themes } from "contexts/ThemeContext";
 
 export default function ThemeContextWrapper(props) {
-  const [theme, setTheme] = useState(themes.dark);
+  const [theme, setTheme] = useState(
+      () => JSON.parse(localStorage.getItem('theme-mode')) || themes.dark
+  );
 
   function changeTheme(theme) {
     setTheme(theme);
   }
 
   useEffect(() => {
+    // Persist the theme-mode using the browser's localStorage.
+    localStorage.setItem('theme-mode', JSON.stringify(theme))
     switch (theme) {
       case themes.light:
         document.body.classList.add("white-content");
