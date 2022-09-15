480c2e174fba424a4442fefa5e75d5964c12d512
majhoolsoft <yousef.fatouraee@aut.ac.ir>
2022-09-16 01:36:07 +0430

    Added fontawsome

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,6 @@
 import '../assets/css/black-dashboard-react.min.css'
 import '../assets/css/nucleo-icons.css'
+import "@fortawesome/fontawesome-free/css/all.min.css";
 import type { AppProps } from 'next/app'
 
 function MyApp({ Component, pageProps }: AppProps) {
