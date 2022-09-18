3fdab62afc0c42a82121c776694768998c7ac872
majhoolsoft <yousef.fatouraee@aut.ac.ir>
2022-09-18 18:30:38 +0430

    Added theme and background contexts

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,8 +3,16 @@ import '../assets/css/nucleo-icons.css'
 import "@fortawesome/fontawesome-free/css/all.min.css";
 import type { AppProps } from 'next/app'
 import AdminLayout from "../layouts/Admin/Admin"
+
+import ThemeContextWrapper from "../components/ThemeWrapper/ThemeWrapper";
+import BackgroundColorWrapper from "../components/BackgroundColorWrapper/BackgroundColorWrapper";
 function MyApp({ Component, pageProps }: AppProps) {
-  return <AdminLayout><Component {...pageProps} /></AdminLayout>
+  return (
+  <ThemeContextWrapper>
+    <BackgroundColorWrapper>
+      <AdminLayout><Component {...pageProps} /></AdminLayout>
+    </BackgroundColorWrapper>
+  </ThemeContextWrapper>)
 }
 
 export default MyApp
