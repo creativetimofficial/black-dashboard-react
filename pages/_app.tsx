a503660ae781e93889f0aa5c7abbe342d840894b
majhoolsoft <yousef.fatouraee@aut.ac.ir>
2022-09-18 13:34:47 +0430

    Added admin layout

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,9 +2,9 @@ import '../assets/css/black-dashboard-react.min.css'
 import '../assets/css/nucleo-icons.css'
 import "@fortawesome/fontawesome-free/css/all.min.css";
 import type { AppProps } from 'next/app'
-
+import AdminLayout from "../layouts/Admin/Admin"
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return <AdminLayout {...pageProps} />
 }
 
 export default MyApp
