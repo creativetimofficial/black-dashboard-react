91d7d9c2f5f4fd0a88c1dd93ba5688170fbcc810
majhoolsoft <yousef.fatouraee@aut.ac.ir>
2022-09-19 17:17:28 +0430

    Added Head related codes and files

---
+++ pages/_document.tsx
@@ -0,0 +1,20 @@
+import { Html, Head, Main, NextScript } from "next/document";
+
+export default function Document() {
+  return (
+    <Html>
+      <Head>
+        <link
+          href="https://fonts.googleapis.com/css?family=Poppins:200,300,400,600,700,800"
+          rel="stylesheet"
+        />
+        {/* <script src="https://maps.googleapis.com/maps/api/js?key=YOUR_KEY_HERE"></script> */}
+      </Head>
+      <body>
+        <noscript> You need to enable JavaScript to run this app. </noscript>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
+}
